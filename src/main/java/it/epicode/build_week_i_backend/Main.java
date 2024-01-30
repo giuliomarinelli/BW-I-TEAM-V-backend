850c8c4a40df62cee9a405d8f0c6cd9c5f809c67
giuliomarinelli <giulio.marinelli@icloud.com>
2024-01-30 11:33:34 +0100

    renamed package

--- src/main/java/it/epicode/build_week_i_backend/Main.java
+++ src/main/java/it/epicode/build_week_i_backend/Main.java
@@ -1,12 +1,10 @@
 package it.epicode.build_week_i_backend;
 
-import it.epicode.build_week_i_backend.Dao.EmittenteDao;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        EmittenteDao emittenteDao = new EmittenteDao();
+//        EmittenteDao emittenteDao = new EmittenteDao();
 
         }
     }
