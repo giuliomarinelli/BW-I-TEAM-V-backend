2ec89e8ff3e0d5b6aca8f65966d3080d90504740
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2024-02-01 16:34:30 +0100

    first commit

--- src/main/java/it/epicode/build_week_i_backend/Dao/UtenteDao.java
+++ src/main/java/it/epicode/build_week_i_backend/Dao/UtenteDao.java
@@ -5,8 +5,12 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UtenteDao {
+
+
     private EntityManagerFactory emf;
     private EntityManager em;
 
@@ -16,33 +20,21 @@ public class UtenteDao {
     }
 
     public void saveUtente(Utente u){
-        try{
         EntityTransaction et = em.getTransaction();
         et.begin();
         em.persist(u);
         et.commit();
-        }catch (NullPointerException e){
-            e.getMessage();
-            em.close();
-            emf.close();
-        }
     }
 
     public Utente getbyid(int id){
         return em.find(Utente.class, id);
     }
         public void deleteUtente(int id){
-        try{
         EntityTransaction et = em.getTransaction();
             et.begin();
             Utente u = getbyid(id);
             em.remove(u);
             et.commit();
-        }catch (NullPointerException e){
-            e.getMessage();
-        }finally {
-            em.close();
-        }
         }
 
 }
